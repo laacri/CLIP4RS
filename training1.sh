d6e222c4b5796438d0336f291c7d6124d6b7470a
unknown <cristiana.ditullioft@gmail.com>
2025-06-12 12:36:09 +0200

    Upload training1 job script

---
+++ training1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#SBATCH -A IsCc7_AdvCMT 
+#SBATCH -p boost_usr_prod
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --job-name="clip_msi1_training_$1"
+#SBATCH --out="./sout/clip_msi1_training_$1.out"
+#SBATCH --open-mode=truncate
+
+echo "Running on nodes: $SLURM_NODELIST"
+echo "Arguments passed: $@"
+
+cd .
+export WANDB_MODE=offline
+module load anaconda3
+module load cuda
+conda init
+
+conda activate test_env
+#source activate test_env
+
+srun python training_clip_msi1.py --max_epochs "$1"
